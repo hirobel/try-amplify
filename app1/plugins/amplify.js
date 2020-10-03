ac4085d5a89d94fb0b6a71665756a13e10cb5333
加藤拓明 <kato.hiroaki@classmethod.jp>
2020-10-04 02:39:29 +0900

    chore: 必要なライブラリを戻す

--- app1/plugins/amplify.js
+++ app1/plugins/amplify.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import awsExports from '../src/aws-exports'
 import '@aws-amplify/ui-vue'
 import Amplify from 'aws-amplify'
-// import Amplify, * as AmplifyModules from 'aws-amplify';
-// import { AmplifyPlugin } from 'aws-amplify-vue';
+import * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
 
 Amplify.configure(awsExports)
-// Vue.use(AmplifyPlugin, AmplifyModules);
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 
 // https://blog.u-chan-chi.com/post/ampilfy-vue-cli/
